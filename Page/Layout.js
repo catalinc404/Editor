9d2d9ea1cd93379807cb004e65d12d8b61918c83
catalinc404 <34883840+catalinc404@users.noreply.github.com>
2018-01-23 16:39:40 +0200

    32

--- Page/Layout.js
+++ Page/Layout.js
@@ -13,8 +13,26 @@ UI.prototype = Object.assign( Object.create( Object.prototype ),
 } );
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+UI.prototype.setupPage = function()
+{
+    console.log( "UI.prototype.setupPage:" );
+    var area = { left: 0, top: 0, width: 1280, height: 960 };
+    this.setupPageLayout( area, this.UIData );
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+UI.prototype.resizePage = function()
+{
+    console.log( "UI.prototype.resizePage:" );
+    var area = { left: 0, top: 0, width: 1280, height: 960 };
+    this.setupPageLayout( area, this.UIData );
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 UI.prototype.setupPageLayout = function( area )
 {
+    console.log( "UI.prototype.setupPageLayout: left=" + area.left + ", top=" + area.top + ", width=" + area.width + ", height=" + area.height );
+
     if( this.UIData === undefined )
     {
         console.log( "UI.prototype.setupPageLayout: undefined layout!" );
@@ -22,7 +40,7 @@ UI.prototype.setupPageLayout = function( area )
         return;
     }
 
-    if( this.UIData.rows === undefined || this.UIData.colons === undefined )
+    if( this.UIData.rows === undefined && this.UIData.colons === undefined )
     {
         console.log( "UI.prototype.setupPageLayout: layout contains no colons or rows!" );
 
@@ -35,30 +53,42 @@ UI.prototype.setupPageLayout = function( area )
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 UI.prototype.setupLayout = function( area, uiData )
 {
+    console.log( "UI.prototype.setupLayout:" );
+
     if( uiData.rows !== undefined )
     {
-        setupLayoutRows( area, uiData.rows );
+        this.setupLayoutRows( area, uiData.rows );
     }
     else
     if( uiData.colons !== undefined )
     {
-        setupLayoutcolons( area, uiData.colons );
+        this.setupLayoutcolons( area, uiData.colons );
     }
     else
     {
-        console.log( "" );
+        console.log( "UI.prototype.setupLayout: no rows or colons defined!" );
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-UI.prototype.setupLayoutRows = function( area, uiData )
+UI.prototype.setupLayoutRows = function( area, rows )
 {
+    console.log( "UI.prototype.setupLayoutRows:" );
+
+    var heightRows = 0
+    for( var i = 0; i < rows.length; ++i) 
+    {
+        heightRows += this.getHeight( rows[i] );
+    }
+
+    
 
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-UI.prototype.setupLayoutColons = function( area, uiData )
+UI.prototype.setupLayoutColons = function( area, colons )
 {
+    console.log( "UI.prototype.setupLayoutColons:" );
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -114,5 +144,8 @@ var pageLayout =
         },
     ] 
 }
+
+
+var ui = new UI( pageLayout );
 
 
