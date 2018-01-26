947a9d5664bf4da6c5e7e51c488c5d67640d0598
catalinc404 <34883840+catalinc404@users.noreply.github.com>
2018-01-26 18:05:35 +0200

    33

--- Page/Layout.js
+++ Page/Layout.js
@@ -62,7 +62,7 @@ UI.prototype.setupLayout = function( area, uiData )
     else
     if( uiData.colons !== undefined )
     {
-        this.setupLayoutcolons( area, uiData.colons );
+        this.setupLayoutColons( area, uiData.colons );
     }
     else
     {
@@ -75,20 +75,203 @@ UI.prototype.setupLayoutRows = function( area, rows )
 {
     console.log( "UI.prototype.setupLayoutRows:" );
 
-    var heightRows = 0
-    for( var i = 0; i < rows.length; ++i) 
+    var heightRows = 0;
+    var heights = [];
+
+    var countUndefinedRows = 0;
+    var countRows = rows.length;
+    for( var i = 0; i < countRows; ++i ) 
     {
-        heightRows += this.getHeight( rows[i] );
+        var height = this.getHeight( rows[i] );
+        heights[i] = height;
+        if( height !== undefined )
+        {
+            heightRows += height;
+        }
+        else
+        {
+            countUndefinedRows = countUndefinedRows + 1;
+        }
     }
 
-    
+    if( countUndefinedRows > 0 )
+    {
+        var heightDefault = Math.max( area.height -  heightRows, 0 ) / countUndefinedRows;
+        for( var i = 0; i < countRows; ++i) 
+        {
+            if( heights[i] === undefined )
+            {
+                heights[i] = heightDefault;
+            }
+        }
+    }
 
+    var currentTop = area.top;
+    for( var i = 0; i < countRows; ++i) 
+    {
+        var newArea = { left: area.left, top: currentTop, width: area.width, height: heights[i] }
+        var element = document.getElementById( rows[i].id );
+        if( element != null )
+        {
+            setElementDimensions( element , newArea.left, newArea.top, newArea.width, newArea.height );
+        }
+        currentTop += heights[i];
+
+        this.setupLayout( newArea, rows[i] );
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 UI.prototype.setupLayoutColons = function( area, colons )
 {
     console.log( "UI.prototype.setupLayoutColons:" );
+
+    var widthColons = 0;
+    var widths = [];
+
+    var countUndefinedColons = 0;
+    var countColons = colons.length;
+    for( var i = 0; i < countColons; ++i ) 
+    {
+        var width = this.getWidth( colons[i] );
+        widths[i] = width;
+        if( width !== undefined )
+        {
+            widthColons += width;
+        }
+        else
+        {
+            countUndefinedColons = countUndefinedColons + 1;
+        }
+    }
+
+    if( countUndefinedColons > 0 )
+    {
+        var widthDefault = Math.max( area.width -  widthColons, 0 ) / countUndefinedColons;
+        for( var i = 0; i < countColons; ++i) 
+        {
+            if( widths[i] === undefined )
+            {
+                widths[i] = widthDefault;
+            }
+        }
+    }
+
+    var currentLeft = area.left;
+    for( var i = 0; i < countColons; ++i) 
+    {
+        var newArea = { left: currentLeft, top: area.top, width: widths[i], height: area.height };
+        var element = document.getElementById( colons[i].id );
+        if( element != null )
+        {
+            setElementDimensions( element , newArea.left, newArea.top, newArea.width, newArea.height );
+        }
+        currentLeft += widths[i];
+
+        this.setupLayout( newArea, colons[i] );
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+UI.prototype.getHeight = function( row )
+{
+    if( row === undefined )
+    {
+        return undefined;
+    }
+    if( row.height !== undefined )
+    {
+        return row.height;
+    }
+
+    if( row.rows !== undefined )
+    {
+        var totalHeight = 0;
+        var countRows = row.rows.length;
+
+        for( var i = 0; i < countRows; ++i )
+        {
+            var height = this.getHeight( row.rows[i] );
+            if( height === undefined )
+            {
+                return undefined;
+            }
+            totalHeight += height;
+        }
+
+        return totalHeight;
+    }
+    else
+    if( row.colons !== undefined )
+    {
+        var totalHeight = 0;
+        var countColons = row.colons.length;
+
+        for( var i = 0; i < countColons; ++i )
+        {
+            var height = this.getHeight( row.colons[i] );
+            if( height === undefined )
+            {
+                return undefined;
+            }
+            totalHeight += height;
+        }
+
+        return totalHeight;
+    }
+
+    return undefined;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+UI.prototype.getWidth = function( colon )
+{
+    if( colon === undefined )
+    {
+        return undefined;
+    }
+    if( colon.width !== undefined )
+    {
+        return colon.width;
+    }
+
+    if( colon.colons !== undefined )
+    {
+        var totalWidth = 0;
+        var countColons = colon.colons.length;
+
+        for( var i = 0; i < countColons; ++i )
+        {
+            var width = this.getWidth( colon.colonss[i] );
+            if( width === undefined )
+            {
+                return undefined;
+            }
+            totalWidth += width;
+        }
+
+        return totalWidth;
+    }
+    else
+    if( colon.rows !== undefined )
+    {
+        var totalWidth = 0;
+        var countColons = row.colons.length;
+
+        for( var i = 0; i < countColons; ++i )
+        {
+            var width = this.getWidth( colon.rows[i] );
+            if( width === undefined )
+            {
+                return undefined;
+            }
+            totalWidth += width;
+        }
+
+        return totalWidth;
+    }
+
+    return undefined;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -102,12 +285,12 @@ var pageLayout =
             [
                 { 
                     name: "header_menu",
-                    id: "",
+                    id: "header",
                     height: 52,
                 },
                 { 
                     name: "header_toolbar",
-                    id: "",
+                    id: "header-tabs",
                     height: 70
                 }
             ]
@@ -122,25 +305,30 @@ var pageLayout =
                     width: 200
                 },
                 {
+                    name: "resizer_left",
+                    id: "resizerLeft",
+                    width: 50
+                },                
+                {
                     name: "body_middle",
                     id: "editor",
                 },
                 {
+                    name: "resizer_right",
+                    id: "resizerRight",
+                    width: 50
+                },                
+                {
                     name: "body_right",
-                    id: "editor",
+                    id: "right",
                     width: 200
                 }
             ]
         },
         {
             name: "footer",
-            cols:
-            [
-                {
-                    name: "footer",
-                    id : "footer",
-                }
-            ]
+            id: "footer",
+            height: 100,
         },
     ] 
 }
